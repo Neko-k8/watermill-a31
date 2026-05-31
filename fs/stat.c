fdc442c47907aba4b9df9c02d91763ebb3e782fb
hakyura <zxmttaa@gmail.com>
2026-05-31 22:16:51 +0700

    Revert "Add missing syscall_hooks for ReSUkiSU"
    This reverts commit 44e87cd11c44a21e04904449be18ad88de8eff43.

--- fs/stat.c
+++ fs/stat.c
@@ -86,6 +86,10 @@ int vfs_fstat(unsigned int fd, struct kstat *stat)
 	return error;
 }
 EXPORT_SYMBOL(vfs_fstat);
+#ifdef CONFIG_KSU
+extern int ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags);
+#endif
+
 
 int vfs_fstatat(int dfd, const char __user *filename, struct kstat *stat,
 		int flag)
@@ -286,17 +290,6 @@ SYSCALL_DEFINE2(newlstat, const char __user *, filename,
 
 	return cp_new_stat(&stat, statbuf);
 }
-
-#ifdef CONFIG_KSU
-__attribute__((hot)) 
-extern int ksu_handle_stat(int *dfd, const char __user **filename_user,
-				int *flags);
-
-extern void ksu_handle_newfstat_ret(unsigned int *fd, struct stat __user **statbuf_ptr);
-#if defined(__ARCH_WANT_STAT64) || defined(__ARCH_WANT_COMPAT_STAT64)
-extern void ksu_handle_fstat64_ret(unsigned long *fd, struct stat64 __user **statbuf_ptr); // optional
-#endif
-#endif
 
 #if !defined(__ARCH_WANT_STAT64) || defined(__ARCH_WANT_SYS_NEWFSTATAT)
 SYSCALL_DEFINE4(newfstatat, int, dfd, const char __user *, filename,
