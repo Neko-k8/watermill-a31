7e0d138952bad9f2c8a1d095c8e4aa3708615039
hakyura <zxmttaa@gmail.com>
2026-06-02 19:32:33 +0700

    Test CPU overclock: big cluster @2.5GHz, little cluster @2.0GHz

--- drivers/misc/mediatek/base/power/cpufreq_v1/src/mach/mt6765/mtk_cpufreq_opp_table.h
+++ drivers/misc/mediatek/base/power/cpufreq_v1/src/mach/mt6765/mtk_cpufreq_opp_table.h
@@ -337,7 +337,7 @@
 
 /* C65 */
 /* for DVFS OPP table L */
-#define CPU_DVFS_FREQ0_L_C65    2301000    /* KHz */
+#define CPU_DVFS_FREQ0_L_C65    2501000    /* KHz */
 #define CPU_DVFS_FREQ1_L_C65    2215000    /* KHz */
 #define CPU_DVFS_FREQ2_L_C65    2139000    /* KHz */
 #define CPU_DVFS_FREQ3_L_C65    2074000    /* KHz */
@@ -355,7 +355,7 @@
 #define CPU_DVFS_FREQ15_L_C65    900000    /* KHz */
 
 /* for DVFS OPP table LL */
-#define CPU_DVFS_FREQ0_LL_C65    1800000    /* KHz */
+#define CPU_DVFS_FREQ0_LL_C65    2000000    /* KHz */
 #define CPU_DVFS_FREQ1_LL_C65    1682000    /* KHz */
 #define CPU_DVFS_FREQ2_LL_C65    1579000    /* KHz */
 #define CPU_DVFS_FREQ3_LL_C65    1491000    /* KHz */
@@ -373,7 +373,7 @@
 #define CPU_DVFS_FREQ15_LL_C65    400000    /* KHz */
 
 /* for DVFS OPP table CCI */
-#define CPU_DVFS_FREQ0_CCI_C65    1051000    /* KHz */
+#define CPU_DVFS_FREQ0_CCI_C65    1262000    /* KHz */
 #define CPU_DVFS_FREQ1_CCI_C65    1000000    /* KHz */
 #define CPU_DVFS_FREQ2_CCI_C65     955000    /* KHz */
 #define CPU_DVFS_FREQ3_CCI_C65     917000    /* KHz */
@@ -391,7 +391,7 @@
 #define CPU_DVFS_FREQ15_CCI_C65    300000    /* KHz */
 
 /* for DVFS OPP table */
-#define CPU_DVFS_VOLT0_VPROC_L_C65    111875          /* 10uV */
+#define CPU_DVFS_VOLT0_VPROC_L_C65    116875          /* 10uV */
 #define CPU_DVFS_VOLT1_VPROC_L_C65    106875          /* 10uV */
 #define CPU_DVFS_VOLT2_VPROC_L_C65    102500          /* 10uV */
 #define CPU_DVFS_VOLT3_VPROC_L_C65     98750          /* 10uV */
@@ -408,7 +408,7 @@
 #define CPU_DVFS_VOLT14_VPROC_L_C65    61875          /* 10uV */
 #define CPU_DVFS_VOLT15_VPROC_L_C65    60000          /* 10uV */
 
-#define CPU_DVFS_VOLT0_VPROC_LL_C65    111875          /* 10uV */
+#define CPU_DVFS_VOLT0_VPROC_LL_C65    116875          /* 10uV */
 #define CPU_DVFS_VOLT1_VPROC_LL_C65    106875          /* 10uV */
 #define CPU_DVFS_VOLT2_VPROC_LL_C65    102500          /* 10uV */
 #define CPU_DVFS_VOLT3_VPROC_LL_C65     98750          /* 10uV */
@@ -425,7 +425,7 @@
 #define CPU_DVFS_VOLT14_VPROC_LL_C65    63125          /* 10uV */
 #define CPU_DVFS_VOLT15_VPROC_LL_C65    60000          /* 10uV */
 
-#define CPU_DVFS_VOLT0_VPROC_CCI_C65    111875          /* 10uV */
+#define CPU_DVFS_VOLT0_VPROC_CCI_C65    116875          /* 10uV */
 #define CPU_DVFS_VOLT1_VPROC_CCI_C65    106875          /* 10uV */
 #define CPU_DVFS_VOLT2_VPROC_CCI_C65    102500          /* 10uV */
 #define CPU_DVFS_VOLT3_VPROC_CCI_C65     98750          /* 10uV */
