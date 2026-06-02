77c6e6664554cc3ce261309ccbf0f5dd813bfd5f
hakyura <zxmttaa@gmail.com>
2026-06-02 18:50:37 +0700

    overclock GPU and disable thermal throttling

--- drivers/misc/mediatek/base/power/mt6765/mtk_gpufreq_core.h
+++ drivers/misc/mediatek/base/power/mt6765/mtk_gpufreq_core.h
@@ -41,15 +41,15 @@
 /**************************************************
  * MT6765 segment_3 : GPU DVFS OPP table Setting
  **************************************************/
-#define SEG3_GPU_DVFS_FREQ0			(680000)	/* KHz */
-#define SEG3_GPU_DVFS_FREQ1			(500000)	/* KHz */
+#define SEG3_GPU_DVFS_FREQ0			(897000)	/* KHz */
+#define SEG3_GPU_DVFS_FREQ1			(600000)	/* KHz */
 #define SEG3_GPU_DVFS_FREQ2			(400000)	/* KHz */
 
-#define SEG3_GPU_DVFS_VOLT0			(80000)		/* mV x 100 */
-#define SEG3_GPU_DVFS_VOLT1			(70000)		/* mV x 100 */
+#define SEG3_GPU_DVFS_VOLT0			(90625)		/* mV x 100 */
+#define SEG3_GPU_DVFS_VOLT1			(75000)		/* mV x 100 */
 #define SEG3_GPU_DVFS_VOLT2			(65000)		/* mV x 100 */
 
-#define SEG3_GPU_DVFS_VSRAM0			(87500)		/* mV x 100 */
+#define SEG3_GPU_DVFS_VSRAM0			(90625)		/* mV x 100 */
 #define SEG3_GPU_DVFS_VSRAM1			(87500)		/* mV x 100 */
 #define SEG3_GPU_DVFS_VSRAM2			(87500)		/* mV x 100 */
 
@@ -139,26 +139,6 @@
 #define MT_GPUFREQ_BATT_OC_PROTECT
 #define MT_GPUFREQ_DYNAMIC_POWER_TABLE_UPDATE
 
-/**************************************************
- * Battery Over Current Protect
- **************************************************/
-#ifdef MT_GPUFREQ_BATT_OC_PROTECT
-#define MT_GPUFREQ_BATT_OC_LIMIT_FREQ			(485000)/* KHz */
-#endif
-
-/**************************************************
- * Battery Percentage Protect
- **************************************************/
-#ifdef MT_GPUFREQ_BATT_PERCENT_PROTECT
-#define MT_GPUFREQ_BATT_PERCENT_LIMIT_FREQ		(485000)/* KHz */
-#endif
-
-/**************************************************
- * Low Battery Volume Protect
- **************************************************/
-#ifdef MT_GPUFREQ_LOW_BATT_VOLT_PROTECT
-#define MT_GPUFREQ_LOW_BATT_VOLT_LIMIT_FREQ		(485000)/* KHz */
-#endif
 
 /**************************************************
  * Proc Node Definition
